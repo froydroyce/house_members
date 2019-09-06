0ce80070505b0d3b8a6245f15ba0b77b2e9a78d1
Sejin Kim <froydroyce@gmail.com>
2019-09-06 11:11:06 -0600

    Install webmock and finish service test

--- spec/services/westeros_service.rb
+++ spec/services/westeros_service.rb
@@ -6,10 +6,16 @@ describe WesterosService do
       json_response = File.open("./fixtures/house_members.json")
       stub_request(:get, "http://westerosapi.herokuapp.com/api/v1/house/greyjoy?api_key=#{ENV['WESTEROS_API']}").
         to_return(status: 200, body: json_response)
-      members = subject.members_by_house('Greyjoy')
-      binding.pry
-      expect()
+      members = subject.members_by_house('greyjoy')
 
+      expect(members).to be_an Hash
+      expect(members.first).to be_a Array
+      expect(members[:data].first[:attributes][:members].count).to eq(7)
+
+      member_data = members[:data].first[:attributes][:members].first
+
+      expect(member_data).to have_key(:id)
+      expect(member_data).to have_key(:name)
     end
   end
 end
